17cd7cc9a15d427d2b21971d248d96389af4881c
Kerem Güneş <k-gun@mail.com>
2016-05-05 09:15:47 +0300

    Complete Froq\Http\Response\Content object.

--- src/Response/Content.php
+++ src/Response/Content.php
@@ -29,10 +29,10 @@ namespace Froq\Http\Response;
  * @object     Froq\Http\Response\Content
  * @author     Kerem Güneş <k-gun@mail.com>
  */
-abstract class Content
+final class Content
 {
     /**
-    * Content types.
+    * Types.
     * @const string
     */
    const TYPE_NONE = 'none',
@@ -41,8 +41,77 @@ abstract class Content
          TYPE_JSON = 'application/json';
 
     /**
-     * Content charset.
+     * Charset.
      * @const string
      */
     const CHARSET_UTF8 = 'utf-8';
+
+    /**
+     * Type.
+     * @var string
+     */
+    private $type;
+
+    /**
+     * Charset.
+     * @var string
+     */
+    private $charset;
+
+    /**
+     * Constructor.
+     * @param string|null $type
+     * @param string|null $charset
+     */
+    final public function __construct(string $type = null, string $charset = null)
+    {
+        if ($type) {
+           $this->setType($type);
+        }
+        if ($charset) {
+            $this->setCharset($charset);
+        }
+    }
+
+    /**
+     * Set type.
+     * @param  string $type
+     * @return self
+     */
+    final public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type.
+     * @return string|null
+     */
+    final public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set charset.
+     * @param  string $charset
+     * @return self
+     */
+    final public function setCharset(string $charset): self
+    {
+        $this->charset = $charset;
+
+        return $this;
+    }
+
+    /**
+     * Get charset.
+     * @param string|null
+     */
+    final public function getCharset()
+    {
+        return $this->charset;
+    }
 }
