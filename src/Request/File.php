3e4ed1a3ec72e4a68990915581681d9eaf489cc4
Kerem Güneş <k-gun@mail.com>
2016-06-24 22:38:12 +0300

    Add missing "use", update errors.

--- src/Request/File.php
+++ src/Request/File.php
@@ -23,6 +23,8 @@ declare(strict_types=1);
 
 namespace Froq\Http\Request;
 
+use Froq\Collection\Collection;
+
 /**
  * @package    Froq
  * @subpackage Froq\Http\Request
@@ -73,11 +75,11 @@ final class File extends Collection
      */
     private static $errors = [
         0 => '',
-        1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
-        2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
-        3 => 'The uploaded file was only partially uploaded',
-        4 => 'No file was uploaded',
-        6 => 'Missing a temporary folder',
+        1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
+        2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.',
+        3 => 'The uploaded file was only partially uploaded.',
+        4 => 'No file was uploaded.',
+        6 => 'Missing a temporary folder.',
         7 => 'Failed to write file to disk.',
         8 => 'A PHP extension stopped the file upload.',
     ];
