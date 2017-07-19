76f0728e2d98ac5b7302d885c30675a144e486d9
Kerem Güneş <k-gun@mail.com>
2017-07-19 21:54:20 +0300

    Add missing statuses.

--- src/Response/Status.php
+++ src/Response/Status.php
@@ -47,73 +47,89 @@ final class Status
      * Informational constants.
      * @const int
      */
-    const CONTINUE                         = 100,
-          SWITCHING_PROTOCOLS              = 101,
-          PROCESSING                       = 102;
+    const CONTINUE                           = 100,
+          SWITCHING_PROTOCOLS                = 101,
+          PROCESSING                         = 102;
 
     /**
      * Success constants.
      * @const int
      */
-    const OK                               = 200,
-          CREATED                          = 201,
-          ACCEPTED                         = 202,
-          NON_AUTHORITATIVE_INFORMATION    = 203,
-          NO_CONTENT                       = 204,
-          RESET_CONTENT                    = 205,
-          PARTIAL_CONTENT                  = 206,
-          MULTI_STATUS                     = 207,
-          ALREADY_REPORTED                 = 208,
-          IM_USED                          = 226;
+    const OK                                 = 200,
+          CREATED                            = 201,
+          ACCEPTED                           = 202,
+          NON_AUTHORITATIVE_INFORMATION      = 203,
+          NO_CONTENT                         = 204,
+          RESET_CONTENT                      = 205,
+          PARTIAL_CONTENT                    = 206,
+          MULTI_STATUS                       = 207,
+          ALREADY_REPORTED                   = 208,
+          IM_USED                            = 226;
 
     /**
      * Redirection constants.
      * @const int
      */
-    const MULTIPLE_CHOICES                 = 300,
-          MOVED_PERMANENTLY                = 301,
-          FOUND                            = 302,
-          SEE_OTHER                        = 303,
-          NOT_MODIFIED                     = 304,
-          USE_PROXY                        = 305,
-          TEMPORARY_REDIRECT               = 307,
-          PERMANENT_REDIRECT               = 308;
+    const MULTIPLE_CHOICES                   = 300,
+          MOVED_PERMANENTLY                  = 301,
+          FOUND                              = 302,
+          SEE_OTHER                          = 303,
+          NOT_MODIFIED                       = 304,
+          USE_PROXY                          = 305,
+          TEMPORARY_REDIRECT                 = 307,
+          PERMANENT_REDIRECT                 = 308;
 
     /**
      * Client error constants.
      * @const int
      */
-    const BAD_REQUEST                      = 400,
-          UNAUTHORIZED                     = 401,
-          PAYMENT_REQUIRED                 = 402,
-          FORBIDDEN                        = 403,
-          NOT_FOUND                        = 404,
-          METHOD_NOT_ALLOWED               = 405,
-          NOT_ACCEPTABLE                   = 406,
-          PROXY_AUTHENTICATION_REQUIRED    = 407,
-          REQUEST_TIMEOUT                  = 408,
-          CONFLICT                         = 409,
-          GONE                             = 410,
-          LENGTH_REQUIRED                  = 411,
-          PRECONDITION_FAILED              = 412,
-          REQUEST_ENTITY_TOO_LARGE         = 413,
-          REQUEST_URI_TOO_LONG             = 414,
-          UNSUPPORTED_MEDIA_TYPE           = 415,
-          REQUESTED_RANGE_NOT_SATISFIABLE  = 416,
-          EXPECTATION_FAILED               = 417,
-          I_M_A_TEAPOT                     = 418;
+    const BAD_REQUEST                        = 400,
+          UNAUTHORIZED                       = 401,
+          PAYMENT_REQUIRED                   = 402,
+          FORBIDDEN                          = 403,
+          NOT_FOUND                          = 404,
+          METHOD_NOT_ALLOWED                 = 405,
+          NOT_ACCEPTABLE                     = 406,
+          PROXY_AUTHENTICATION_REQUIRED      = 407,
+          REQUEST_TIMEOUT                    = 408,
+          CONFLICT                           = 409,
+          GONE                               = 410,
+          LENGTH_REQUIRED                    = 411,
+          PRECONDITION_FAILED                = 412,
+          PAYLOAD_TOO_LARGE                  = 413,
+          REQUEST_URI_TOO_LONG               = 414,
+          UNSUPPORTED_MEDIA_TYPE             = 415,
+          REQUESTED_RANGE_NOT_SATISFIABLE    = 416,
+          EXPECTATION_FAILED                 = 417,
+          I_M_A_TEAPOT                       = 418,
+          MISDIRECTED_REQUEST                = 421,
+          UNPROCESSABLE_ENTITY               = 422,
+          LOCKED                             = 423,
+          FAILED_DEPENDENCY                  = 424,
+          UPGRADE_REQUIRED                   = 426,
+          PRECONDITION_REQUIRED              = 428,
+          TOO_MANY_REQUESTS                  = 429,
+          REQUEST_HEADER_FIELDS_TOO_LARGE    = 431,
+          CONNECTION_CLOSED_WITHOUT_RESPONSE = 444,
+          UNAVAILABLE_FOR_LEGAL_REASONS      = 451,
+          CLIENT_CLOSED_REQUEST              = 499;
 
     /**
      * Server error constants.
      * @const int
      */
-    const INTERNAL_SERVER_ERROR            = 500,
-          NOT_IMPLEMENTED                  = 501,
-          BAD_GATEWAY                      = 502,
-          SERVICE_UNAVAILABLE              = 503,
-          GATEWAY_TIMEOUT                  = 504,
-          HTTP_VERSION_NOT_SUPPORTED       = 505,
-          BANDWIDTH_LIMIT_EXCEEDED         = 509;
+    const INTERNAL_SERVER_ERROR              = 500,
+          NOT_IMPLEMENTED                    = 501,
+          BAD_GATEWAY                        = 502,
+          SERVICE_UNAVAILABLE                = 503,
+          GATEWAY_TIMEOUT                    = 504,
+          HTTP_VERSION_NOT_SUPPORTED         = 505,
+          VARIANT_ALSO_NEGOTIATES            = 506,
+          INSUFFICIENT_STORAGE               = 507,
+          LOOP_DETECTED                      = 508,
+          NOT_EXTENDED                       = 510,
+          NETWORK_AUTHENTICATION_REQUIRED    = 511,
+          NETWORK_CONNECT_TIMEOUT_ERROR      = 599;
 
     /**
      * Code.
@@ -170,12 +186,23 @@ final class Status
         410 => 'Gone',
         411 => 'Length Required',
         412 => 'Precondition Failed',
-        413 => 'Request Entity Too Large',
+        413 => 'Payload Too Large',
         414 => 'Request-URI Too Long',
         415 => 'Unsupported Media Type',
         416 => 'Requested Range Not Satisfiable',
         417 => 'Expectation Failed',
         418 => 'I\'m a Teapot',
+        421 => 'Misdirected Request',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        444 => 'Connection Closed Without Response',
+        451 => 'Unavailable For Legal Reasons',
+        499 => 'Client Closed Request',
 
         // server error 5xx
         500 => 'Internal Server Error',
@@ -184,7 +211,12 @@ final class Status
         503 => 'Service Unavailable',
         504 => 'Gateway Timeout',
         505 => 'HTTP Version Not Supported',
-        509 => 'Bandwidth Limit Exceeded',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+        599 => 'Network Connect Timeout Error'
     ];
 
     /**
@@ -194,7 +226,7 @@ final class Status
      */
     final public function __construct(int $code = self::DEFAULT_CODE, string $text = null)
     {
-        if ($text == '') {
+        if ($text == null) {
             $text = self::getTextByCode($code);
         }
 
@@ -279,7 +311,7 @@ final class Status
      */
     final public static function getCodeByText(string $text)
     {
-        return (array_flip(self::$statuses)[$text] ?? null);
+        return array_flip(self::$statuses)[$text] ?? null;
     }
 
     /**
@@ -289,6 +321,6 @@ final class Status
      */
     final public static function getTextByCode(int $code)
     {
-        return (self::$statuses[$code] ?? null);
+        return self::$statuses[$code] ?? null;
     }
 }
