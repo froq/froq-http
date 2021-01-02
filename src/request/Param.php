a82df1c168e7b6b06fe5fc821188383d385abed2
Kerem Güneş <qeremy@gmail.com>
2021-01-03 01:15:34 +0300

    request.Param: add trim arg & make trim() as default map function.

--- src/request/Param.php
+++ src/request/Param.php
@@ -28,12 +28,16 @@ final class Param extends StaticClass
      * @param  any|null      $default
      * @param  callable|null $map
      * @param  callable|null $filter
+     * @param  bool          $trim
      * @param  bool          $combine
      * @return any
      */
     public static function get(string|array $name, $default = null, callable $map = null, callable $filter = null,
-        bool $combine = false)
+        bool $trim = true, bool $combine = false)
     {
+        // Trim is default for map if not false.
+        if ($trim && !$map) $map = 'trim';
+
         $values = Params::gets((array) $name, $default);
 
         if ($map || $filter) {
@@ -56,12 +60,16 @@ final class Param extends StaticClass
      * @param  any|null      $default
      * @param  callable|null $map
      * @param  callable|null $filter
+     * @param  bool          $trim
      * @param  bool          $combine
      * @return any
      */
     public static function post(string|array $name, $default = null, callable $map = null, callable $filter = null,
-        bool $combine = false)
+        bool $trim = true, bool $combine = false)
     {
+        // Trim is default for map if not false.
+        if ($trim && !$map) $map = 'trim';
+
         $values = Params::posts((array) $name, $default);
 
         if ($map || $filter) {
@@ -84,13 +92,18 @@ final class Param extends StaticClass
      * @param  any|null      $default
      * @param  callable|null $map
      * @param  callable|null $filter
+     * @param  bool          $trim
      * @param  bool          $combine
      * @return any
      */
     public static function cookie(string|array $name, $default = null, callable $map = null, callable $filter = null,
-        bool $combine = false)
+        bool $trim = true, bool $combine = false)
     {
+        // Trim is default for map if not false.
+        if ($trim && !$map) $map = 'trim';
+
         $values = Params::cookies((array) $name, $default);
+
         if ($map || $filter) {
             $values = self::applyMapFilter($values, $map, $filter);
         }
@@ -107,13 +120,13 @@ final class Param extends StaticClass
     /**
      * Apply map/filter.
      *
-     * @param  array         $values
-     * @param  callable|null $map
-     * @param  callable|null $filter
+     * @param  array     $values
+     * @param  ?callable $map
+     * @param  ?callable $filter
      * @return array
      * @internal
      */
-    private static function applyMapFilter(array $values, callable $map = null, callable $filter = null): array
+    private static function applyMapFilter(array $values, ?callable $map, ?callable $filter): array
     {
         // Apply map & filter if provided.
         $map    && $values = array_map($map, $values);
