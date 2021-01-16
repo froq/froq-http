552c2328f82315344704efd0a422772d12d44616
Kerem Güneş <qeremy@gmail.com>
2021-01-16 03:02:02 +0300

    client.Request: ksort() headers.

--- src/client/Request.php
+++ src/client/Request.php
@@ -54,6 +54,7 @@ final class Request extends Message
         // Merge & normalize headers.
         $headers = array_replace_recursive($headersDefault, $headers ?? []);
         $headers = array_change_key_case($headers, CASE_LOWER);
+        ksort($headers);
 
         parent::__construct(Message::TYPE_REQUEST, null, $headers, $body);
     }
