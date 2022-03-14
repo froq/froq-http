e94bd85f0f0eb695956c0ee3061fe301844e3b20
Kerem Güneş <qeremy@gmail.com>
2022-03-14 05:46:13 +0300

    * common.HeaderTrait: fix getHeader() [default issue].

--- src/common/HeaderTrait.php
+++ src/common/HeaderTrait.php
@@ -103,8 +103,9 @@ trait HeaderTrait
      */
     public function getHeader(string $name, string|array $default = null): string|array|null
     {
-        return $this->headers->get($name, $default)
-            ?? $this->headers->get(strtolower($name), $default);
+        return $this->headers->get($name)
+            ?? $this->headers->get(strtolower($name))
+            ?? $default;
     }
 
     /**
